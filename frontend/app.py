246e9e264564ebedb28d4d962a8a735c525119d8
Nilay <rawalnilay1@gmail.com>
2024-03-06 23:43:04 +0530

    crop recomm pickle model

--- frontend/app.py
+++ frontend/app.py
@@ -47,6 +47,10 @@ def signup():
 def success():
     return render_template('404.html')
 
+@app.route('/bootstrap')
+def bootstrap():
+    return render_template('https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css')
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
