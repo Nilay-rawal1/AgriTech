47ee02e1297352489dee5357173a65a066b53b06
Addy2004 <40354792+Addy2004@users.noreply.github.com>
2024-03-07 14:29:53 +0530

    Major refactoring and bugfixes in login and anchor linking

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db = SQLAlchemy(app)
 
@@ -11,12 +13,33 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
 
-@app.route('/')
+@app.route("/home", endpoint="home()")
 def home():
     return render_template('home.html')
 
-@app.route('/login', methods=['POST'])
+@app.route("/team", endpoint="team()")
+def team():
+    return render_template('team.html')
+
+
+@app.route("/login", endpoint="login()")
 def login():
+    return render_template('login.html')
+
+@app.route("/services", endpoint="services()")
+def services():
+    return render_template('services.html')
+
+@app.route("/weather", endpoint="weather()")
+def services():
+    return render_template('weather.html')
+
+@app.route("/404", endpoint="e404()")
+def e404():
+    return render_template('404.html')
+
+@app.route('/auth', endpoint="auth()", methods=['POST'])
+def auth():
     email = request.form['Login-Email']
     password = request.form['Login-Password']
     
@@ -45,13 +68,10 @@ def signup():
 
 @app.route('/success')
 def success():
-    return render_template('404.html')
-
-@app.route('/bootstrap')
-def bootstrap():
-    return render_template('https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css')
+    return render_template('home.html')
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+
