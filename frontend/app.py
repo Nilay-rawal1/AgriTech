42dacd6bce76b180528bb17be89502cc39863b1d
Nilay <rawalnilay1@gmail.com>
2024-03-10 20:01:24 +0530

    prediciton logging

--- frontend/app.py
+++ frontend/app.py
@@ -1,3 +1,5 @@
+import joblib
+import numpy
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
@@ -78,6 +80,32 @@ def signup():
 @app.route('/success')
 def success():
     return render_template('home.html')
+
+
+# model
+
+@app.route('/form', methods=["POST"])
+def brain():
+    Nitrogen=float(request.form['Nitrogen'])
+    Phosphorus=float(request.form['Phosphorus'])
+    Potassium=float(request.form['Potassium'])
+    Temperature=float(request.form['Temperature'])
+    Humidity=float(request.form['Humidity'])
+    Ph=float(request.form['ph'])
+    Rainfall=float(request.form['Rainfall'])
+     
+    values=[Nitrogen,Phosphorus,Potassium,Temperature,Humidity,Ph,Rainfall]
+    
+    if Ph>0 and Ph<=14 and Temperature<100 and Humidity>0:
+        joblib.load('static/crop_app','r')
+        model = joblib.load(open('static/crop_app','rb'))
+        arr = [values]
+        acc = model.predict(arr)
+        # print(acc)
+        return render_template('prediction.html', prediction=str(acc))
+    else:
+        return "Sorry...  Error in entered values in the form Please check the values and fill it again"
+
 
 if __name__ == '__main__':
     with app.app_context():
